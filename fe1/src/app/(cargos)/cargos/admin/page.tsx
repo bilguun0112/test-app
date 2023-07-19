b0d37b3765b25dda4d9a79c569a2317988c55b4b
bilguun0112 <bilguunbayanmunkh10@gmail.com>
2023-07-19 22:15:15 +0800

    s

--- fe1/src/app/(cargos)/cargos/admin/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function AdminPage(): JSX.Element {
-  return (
-    <div>
-      <h1>Admin Panel</h1>
-    </div>
-  );
-}
--- fe1/src/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,5 +0,0 @@
-import { authOptions } from "@/utils/authOptions";
-import NextAuth from "next-auth";
-
-const authHandler = NextAuth(authOptions);
-export { authHandler as GET, authHandler as POST };
